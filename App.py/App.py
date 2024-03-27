9ed85c2c78c009cc910022260ac5e66d3e4db329
Matheus-Alechandre <162439808+Matheus-Alechandre@users.noreply.github.com>
2024-03-27 08:37:10 -0300

    Add files via upload

---
+++ App.py/App.py
@@ -0,0 +1,10 @@
+import os
+
+print("""Espetinho do Jorjão """)
+print("1. Cadastrar Restaurante")
+print("2. Listar Restaurante")
+print("3. Ativar restaurante")
+print("4. Sair")
+
+opcao_escolhida = int(input('Escolha uma opção'))
+#print('Voce escolheu a opção', opcao_escolhida)
